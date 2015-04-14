e1ab557cee16d0c25bbb775e0b64db4854224ddd
NSSektor <angel.sektor@gmail.com>
2015-04-14 16:19:32 -0500

    Initial Commit

---
+++ Lock8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lock8
+//
+//  Created by Angel Rivas on 4/14/15.
+//  Copyright (c) 2015 Tecnologizame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lock8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lock8
+//
+//  Created by Angel Rivas on 4/14/15.
+//  Copyright (c) 2015 Tecnologizame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
